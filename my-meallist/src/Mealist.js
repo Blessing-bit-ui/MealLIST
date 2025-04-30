50db15ac5208d741d662a857f19665433c814690
Blessing-bit-ui <yembeblessing27@gmail.com>
2025-04-30 14:27:56 -0700

    created content in mealist

--- my-meallist/src/Mealist.js
+++ my-meallist/src/Mealist.js
@@ -6,6 +6,33 @@ function Mealist(){
         {name:"Grilled Veggies", calories:"80"},
         {name:"Soup", calories:"100"}
     ]
-    const [meals, setMeals]=useState(todayMeals)
-
+    const [meals, setMeals]=useState(todayMeals);
+const [status, setStatus]=useState(Array(todayMeals.length).fill("Eat"));
+const[count, setCount]=useState(todayMeals.length);
+const toggleStatus=(index)=>{
+    const newStatus=[...status]
+    if(newStatus[index]==="Eat"){
+        newStatus[index]="Eaten";
+        setCount(count-1)
+    }else{
+        newStatus[index] ="Eat"
+        setCount(count + 1)
+    }
+    setStatus(newStatus);
 }
+return (
+  <>
+    {meals.map((meal, index) => (
+      <div key={index}>
+        <h1>
+          {meal.name}
+          {meal.calories}
+        </h1>
+        <button onClick={() => toggleStatus(index)}>{status[index]} </button>
+      </div>
+    ))}
+    <p> Meal remaining={count}</p>
+  </>
+);
+}
+export default Mealist;
